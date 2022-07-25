e870dd4a00647ff13b3d139ccd93d14d78be0fec
Tom van Enckevort <tom@happyporch.com>
2022-07-25 15:01:35 +0100

    Replaced underscore in template name

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-mailing-list-user-preferences
 # about: Plugin that adds a new section to the user preferences in Discourse where the user can manage the notifications for all available categories.
-# version: 0.1.1
+# version: 0.1.2
 # authors: HappyPorch
 # url: https://github.com/HappyPorch/discourse-mailing-list-user-preferences/
 
